c63dc72ae7175a17127453708670cd9cf7a5fa97
Théo Mestries <theo.203@hotmail.com>
2023-04-08 23:06:59 +0200

    Add the use of the button ( BaseViewModel.cs , MainPage.xaml , Page1.xaml, Page1.xaml.cs , Page1ViewModel.cs)

---
+++ TimotheeTheoSpotifyApp/TimotheeTheoSpotifyApp/ViewModel/Page1ViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+using XXXXXX;
+
+namespace TimotheeTheoSpotifyApp.ViewModel
+{
+    public class Page1ViewModel : BaseViewModel
+    {
+
+        // Propriétés accessibles pour la liaison de données
+        public Page1ViewModel()
+        {
+            Color1 = Color.Green;
+            Color2 = Color.CornflowerBlue;
+        }
+        public Color Color1 
+        {
+            get
+            {
+                return GetValue<Color>();
+            }
+            set
+            {
+                SetValue(value);
+            }
+        }
+
+        public Color Color2
+        {
+            get
+            {
+                return GetValue<Color>();
+            }
+            set
+            {
+                SetValue(value);
+            }
+        }
+        
+
+        // Méthode pour inverser les couleurs
+
+
+        public void InvertColors()
+        {
+            if (Color1 == Color.CornflowerBlue)
+            {
+                Color1 = Color.Green;
+                Color2 = Color.CornflowerBlue;
+            }
+            else
+            {
+                Color1 = Color.CornflowerBlue;
+                Color2 = Color.Green;
+            }
+            
+            
+        }
+
+        
+
+        
+    }
+}
--- TimotheeTheoSpotifyApp/TimotheeTheoSpotifyApp/views/Page1.xaml.cs
+++ TimotheeTheoSpotifyApp/TimotheeTheoSpotifyApp/views/Page1.xaml.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using TimotheeTheoSpotifyApp.ViewModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +8,19 @@ namespace TimotheeTheoSpotifyApp.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Page1 : ContentPage
     {
+        private Page1ViewModel viewModel;
+
         public Page1()
         {
             InitializeComponent();
+
+            viewModel = new Page1ViewModel();
+            BindingContext = viewModel;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            viewModel.InvertColors();
         }
     }
-}
+    }
